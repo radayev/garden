ed5d3a3699a3c65b15df78713a9d73e38cbf9fed
Alex <radayev@icloud.com>
2020-04-05 23:43:25 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.http import Http404
+from django.conf import settings
 import git
 import json
 # Create your views here.
@@ -15,15 +16,15 @@ def userView(request):
 
 @csrf_exempt
 def gitPull(request):
-	# if request.method == 'POST':
-	if settings.DEBUG:
-		repo = git.Repo(settings.BASE_DIR)
-		origin = repo.remotes.origin
-		origin.pull()
+	if request.method == 'POST':
+		if settings.DEBUG:
+			repo = git.Repo(settings.BASE_DIR)
+			origin = repo.remotes.origin
+			origin.pull()
 
-		os.system('touch /var/www/ctavares94_pythonanywhere_com_wsgi.py')
-		return HttpResponse('Updated successfully')
-		# else:
-		# 	return HttpResponse('Error')
+			os.system('touch /var/www/ctavares94_pythonanywhere_com_wsgi.py')
+			return HttpResponse('Updated successfully')
+		else:
+			return HttpResponse('Error')
 	raise Http404()
 	return HttpResponseRedirect(reverse_lazy('404'))
